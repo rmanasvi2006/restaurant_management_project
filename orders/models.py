c4e590ec97a2da037a1de7dfa5bdefd89546d5d8
rmanasvi2006 <manasvirapolu@gmail.com>
2025-10-07 20:53:55 +0530

    50113

--- orders/models.py
+++ orders/models.py
@@ -2,7 +2,20 @@ from django.db import models
 
 # Create your models here.
 class OrderStatus (models.Model):
-    name = models.CharField(max_length=50,unique=True)
+    PENDING = "pending"
+    PROCESSING = "processing"
+    COMPLETED = "completed"
+    CANCELED = "canceled"
+
+    STATUS_CHOICES = [
+        (PENDING,"pending"),
+        (PROCESSING,"processing"),
+        (COMPLETED,"completed"),
+        (CANCELED,"canceled"),
+    ]
+        ]
+    name = models.CharField(max_length=50,unique=True,choices=STATUS_CHOICES)
+    
     def __str__(self):
         return self.name
 
